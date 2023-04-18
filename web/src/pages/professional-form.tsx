240b2ca8499480861e6573d670c61c759bc6caf8
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-04-18 12:50:44 -0300

    professional form first steps i stopped it to do the service first, service is necessary for the professional tab to exist

---
+++ web/src/pages/professional-form.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function InformationTab(){}
+function ServicesTab(){}
+function DisponibilityTab(){}
+function DeleteTab(){}
+
+function ProfessionalForm(){
+    const [tab, setTab] = useState(0);
+
+
+}
