750f14a1026748e7a3ce0a862a330d8e3f307adf
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-04-20 19:27:20 -0300

    react router implementation

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import { ServiceForm } from './pages/service-form/service-form';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ServiceList } from "./pages/service/service-list/service-list";
+import ServiceForm from "./pages/service/service-form/service-form";
+import { ServiceAdd } from "./pages/service/service-add/service-add";
+import { ServiceEdit } from "./pages/service/service-edit/service-edit";
+import ProfessionalForm from "./pages/professional/professional-form/professional-form";
 
 function App() {
+
+	const routes = [
+		{ path: '/service', element: <ServiceList /> },
+		{ path: '/service/add', element: <ServiceAdd /> },
+		{ path: '/service/edit/:serviceId', element: <ServiceEdit /> },
+
+		{ path: '/professional', element: <ProfessionalForm /> },
+		{ path: '/professional/add', element: <ServiceEdit /> },
+		{ path: '/professional/edit/:professionalId', element: <ServiceEdit /> },
+
+	]
+
 	return (
-		<div className="App">
-			<ServiceForm />
-		</div>
+		<BrowserRouter>
+			<Routes>
+				{
+					routes.map((route) => {
+						return <Route path={route.path} element={route.element}></Route>
+					})
+				}
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
