376f17ebb0f2d4f6a5676148cefc31c1bbee4a27
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-04-20 19:29:09 -0300

    config controller created

--- web/src/controllers/configController.ts
+++ web/src/controllers/configController.ts
@@ -1,9 +1,27 @@
-type configType = {
-    colors:{
-        primary: string,
-        secondary: string,
-        terciary:string,
-    }
+import { doc, getDoc, setDoc } from "firebase/firestore"
+import { db } from "../firebase/firebase"
+
+type colorsType = {
+    primary: string,
+    secondary: string,
+    terciary: string,
+}
+export type idsType = {
+    service: number,
+    occupation:number
 }
 
-export { }
+async function getIds(): Promise<idsType> {
+    /**
+     * Grab and return the current id values
+     */
+    const docRef = doc(db, 'config', 'ids');
+    const docSnap = await getDoc(docRef);
+    return docSnap.data() as idsType
+}
+async function setIds(ids: idsType) {
+    const docRef = doc(db, 'config', 'ids')
+    setDoc(docRef, ids)
+}
+
+export { getIds, setIds }
