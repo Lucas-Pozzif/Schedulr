0860ca83a43ed4138d8f74781ce6bb9c4ae9ed4c
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-04-21 21:50:52 -0300

    global designs update

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,5 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles/index.css'
+import './styles/reset.css'
+
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
