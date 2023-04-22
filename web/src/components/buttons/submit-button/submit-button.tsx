f4fd54ece4c1191d811e62430312465dd3796902
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-04-21 21:51:34 -0300

    submit button complete

---
+++ web/src/components/buttons/submit-button/submit-button.tsx
@@ -0,0 +1,18 @@
+import './submit-button.css'
+
+type submitButtonType = {
+    hide: boolean,
+    title: string,
+    onClickButton: () => void
+
+}
+
+export function SubmitButton({ hide, title, onClickButton }: submitButtonType) {
+
+    return (
+        <div className={`submit-button ${hide ? 'button-hide' : ''}`} onClick={onClickButton}>
+            <p className='title'>{title}</p>
+        </div>
+    )
+
+}
