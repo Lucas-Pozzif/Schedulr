d7a65c2ae97f3b03ee7e0bf80b055b8c02f73191
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-04-21 21:52:27 -0300

    config controller now gets design

--- web/src/controllers/configController.ts
+++ web/src/controllers/configController.ts
@@ -1,6 +1,8 @@
 import { doc, getDoc, setDoc } from "firebase/firestore"
 import { db } from "../firebase/firebase"
 
+let designCache = require('../cache/designCache.json')
+
 type colorsType = {
     primary: string,
     secondary: string,
@@ -8,8 +10,8 @@ type colorsType = {
 }
 export type idsType = {
     service: number,
-    occupation:number,
-    professional:number
+    occupation: number,
+    professional: number
 }
 
 async function getIds(): Promise<idsType> {
@@ -20,9 +22,17 @@ async function getIds(): Promise<idsType> {
     const docSnap = await getDoc(docRef);
     return docSnap.data() as idsType
 }
+
 async function setIds(ids: idsType) {
     const docRef = doc(db, 'config', 'ids')
     setDoc(docRef, ids)
 }
 
-export { getIds, setIds }
+async function getDesigns() {
+    const docRef = doc(db, 'config', 'design');
+    const docSnap = await getDoc(docRef);
+
+    designCache['0'] = docSnap.data();
+}
+
+export { getIds, setIds, getDesigns }
