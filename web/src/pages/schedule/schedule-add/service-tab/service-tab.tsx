d567ed97ebadd78b0a50ff4ce8bd4177b8e74e87
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-04-24 22:25:00 -0300

    service tab update

--- web/src/pages/schedule/schedule-add/service-tab/service-tab.tsx
+++ web/src/pages/schedule/schedule-add/service-tab/service-tab.tsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import { scheduleTabType, selectedServiceType } from "../schedule-add";
 import { getAllServices } from "../../../../controllers/serviceController";
-import { LargeButton } from "../../../../components/buttons/large-button/large-button";
 import { ServiceButton } from "../../../../components/buttons/service-button/service-button";
+
+import './service-tab.css'
 
 const serviceCache = require('../../../../cache/serviceCache.json')
 
@@ -17,84 +18,83 @@ export function ServiceTab({ schedule, setSchedule }: scheduleTabType) {
         });
     }, []);
 
+    function onClickExpandedHandler(serviceId: string) {
+        var functions: [() => void, () => void, () => void, () => void] = [() => { }, () => { }, () => { }, () => { }]
+
+        var selectedServices = [...schedule.selectedServices]
+        const filteredService = selectedServices.filter((selectedService) => selectedService.service === parseInt(serviceId))
+        const newSelectedService: selectedServiceType = {
+            service: parseInt(serviceId),
+            state: 0,
+            professional: null,
+            startTime: null
+        }
+
+        for (let i = 0; i < 4; i++) {
+            functions[i] = (() => {
+                newSelectedService.state = i
+                filteredService.length <= 0 ?
+                    selectedServices.push(newSelectedService) :
+                    selectedServices = selectedServices.filter((selectedService) => selectedService.service !== parseInt(serviceId))
+                setSchedule({
+                    ...schedule,
+                    selectedServices: selectedServices
+                });
+            })
+        }
+        return functions
+    }
+    function onClickHandler(serviceId: string) {
+        const service = serviceCache[serviceId]
+        let selectedServices = [...schedule.selectedServices]
+        const filteredService = selectedServices.filter((selectedService) => selectedService.service === parseInt(serviceId))
+        const newSelectedService: selectedServiceType = {
+            service: parseInt(serviceId),
+            state: 0,
+            professional: null,
+            startTime: null
+        }
+
+        if (service.haveStates) return
+        filteredService.length <= 0 ?
+            selectedServices.push(newSelectedService) :
+            selectedServices = selectedServices.filter((selectedService) => selectedService.service !== parseInt(serviceId))
+        setSchedule({
+            ...schedule,
+            selectedServices: selectedServices
+        });
+
+    }
+    function expandedSelectedList(serviceId: string): [boolean, boolean, boolean, boolean] {
+        let selectedServices = [...schedule.selectedServices]
+        const filteredService = selectedServices.filter((selectedService) => selectedService.service === parseInt(serviceId))
+
+        return [
+            filteredService[0]?.state === 0,
+            filteredService[0]?.state === 1,
+            filteredService[0]?.state === 2,
+            filteredService[0]?.state === 3,
+        ]
+
+    }
+
     return (
-        <div>
+        <div className="service-tab">
             {
                 loading ?
                     <p>loading...</p> :
                     serviceIds!.map((serviceId: string) => {
-                        const service = serviceCache[serviceId]
                         let selectedServices = [...schedule.selectedServices]
+                        const service = serviceCache[serviceId]
                         const filteredService = selectedServices.filter((selectedService) => selectedService.service === parseInt(serviceId))
-                        const newSelectedService: selectedServiceType = {
-                            service: parseInt(serviceId),
-                            state: 0,
-                            professional: null,
-                            startTime: null
-                        }
+
                         return (
                             <ServiceButton
                                 selected={filteredService.length > 0}
-                                expandedselected={[
-                                    filteredService[0]?.state === 0,
-                                    filteredService[0]?.state === 1,
-                                    filteredService[0]?.state === 2,
-                                    filteredService[0]?.state === 3,
-                                ]}
+                                expandedselected={expandedSelectedList(serviceId)}
                                 service={service}
-                                onClickButton={() => {
-                                    if (service.haveStates) return
-                                    filteredService.length <= 0 ?
-                                        selectedServices.push(newSelectedService) :
-                                        selectedServices = selectedServices.filter((selectedService) => selectedService.service !== parseInt(serviceId))
-                                    setSchedule({
-                                        ...schedule,
-                                        selectedServices: selectedServices
-                                    });
-                                }}
-                                onClickExpanded={[
-                                    () => {
-                                        newSelectedService.state = 0
-                                        filteredService.length <= 0 ?
-                                            selectedServices.push(newSelectedService) :
-                                            selectedServices = selectedServices.filter((selectedService) => selectedService.service !== parseInt(serviceId))
-                                        setSchedule({
-                                            ...schedule,
-                                            selectedServices: selectedServices
-                                        });
-                                    },
-                                    () => {
-                                        newSelectedService.state = 1
-                                        filteredService.length <= 0 ?
-                                            selectedServices.push(newSelectedService) :
-                                            selectedServices = selectedServices.filter((selectedService) => selectedService.service !== parseInt(serviceId))
-                                        setSchedule({
-                                            ...schedule,
-                                            selectedServices: selectedServices
-                                        });
-                                    },
-                                    () => {
-                                        newSelectedService.state = 2
-                                        filteredService.length <= 0 ?
-                                            selectedServices.push(newSelectedService) :
-                                            selectedServices = selectedServices.filter((selectedService) => selectedService.service !== parseInt(serviceId))
-                                        setSchedule({
-                                            ...schedule,
-                                            selectedServices: selectedServices
-                                        });
-                                    },
-                                    () => {
-                                        newSelectedService.state = 3
-                                        filteredService.length <= 0 ?
-                                            selectedServices.push(newSelectedService) :
-                                            selectedServices = selectedServices.filter((selectedService) => selectedService.service !== parseInt(serviceId))
-                                        setSchedule({
-                                            ...schedule,
-                                            selectedServices: selectedServices
-                                        });
-                                    },
-                                ]}
-
+                                onClickButton={() => { onClickHandler(serviceId) }}
+                                onClickExpanded={onClickExpandedHandler(serviceId)}
                             />
                         )
                     })
