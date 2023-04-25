629ad71e270f44ce3f9b8fdeb43770c8133bc047
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-04-25 20:31:59 -0300

    professional button created

--- web/src/components/buttons/professional-button/professional-button.tsx
+++ web/src/components/buttons/professional-button/professional-button.tsx
@@ -1,6 +1,9 @@
 import { professionalType } from "../../../controllers/professionalController"
+import { isSelected } from "../../../functions/is-selected/is-selected"
 
 import './professional-button.css'
+
+const occupationCache = require('../../../cache/occupationCache.json')
 
 type serviceButtonType = {
     selected?: boolean,
@@ -9,22 +12,27 @@ type serviceButtonType = {
     onClickButton: () => void,
 }
 
-export function ProfessionalButton({ selected, professional, rightButtonTitle, onClickButton }: serviceButtonType) {
+export function ProfessionalButton({
+    selected = false,
+    professional,
+    rightButtonTitle,
+    onClickButton
+}: serviceButtonType) {
     const title = professional.name
-    const image = professional.photo
-    const subtitle = professional.services
+    const image = professional.photo 
+    const subtitle = professional.occupations.map(occupationId => occupationCache[occupationId].name)
 
     return (
-        <>
-            <div className={`professional-button${selected ? '-selected' : ''}`}>
-                <img className={`image${selected ? '-selected' : ''}`} src={image} />
-                <p className={`title${selected ? '-selected' : ''}`}>{title}</p>
-                <p className={`subtitle${selected ? '-selected' : ''}`}>{subtitle}</p>
-                <div className={`right-button${selected ? '-selected' : ''}`}>
-                    <p className={`right-button-title${selected ? '-selected' : ''}`}>{rightButtonTitle}</p>
+        <div className={`professional-button button ${isSelected(selected)}`} onClick={onClickButton}>
+            <div className="pb-left-block">
+                <img className={`pb-image button ${isSelected(!selected)}`} src={image}></img>
+                <div className={`pb-title-block`}>
+                    <p className={`pb-title button-text ${isSelected(selected)}`}>{title}</p>
+                    <p className={`pb-subtitle button button-text ${isSelected(!selected)}`}>{subtitle}</p>
                 </div>
 
             </div>
-        </>
+            <p className={`pb-right-button-title button button-text ${isSelected(!selected)}`}>{rightButtonTitle}</p>
+        </div >
     )
 }
