77680d2b1a6242b53356c5a7c5f5ea9cf8d417d7
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-04-24 22:24:49 -0300

    daytab update

--- web/src/pages/schedule/schedule-add/day-tab/day-tab.tsx
+++ web/src/pages/schedule/schedule-add/day-tab/day-tab.tsx
@@ -1,33 +1,26 @@
 import { DateButton } from "../../../../components/buttons/date-button/date-button";
-import { LargeButton } from "../../../../components/buttons/large-button/large-button";
+import { dateArrayGenerator } from "../../../../functions/date-time/date-array-generator";
 import { scheduleTabType } from "../schedule-add";
 
+import './day-tab.css'
+
 export function DayTab({ schedule, setSchedule }: scheduleTabType) {
-    const today = new Date();
-    const dates = [];
-    for (let i = 0; i < 30; i++) {
-        const date = new Date();
-        date.setDate(today.getDate() + i);
-        dates.push(date);
-    }
+    const dates = dateArrayGenerator(30)
 
     return (
         <div className="day-tab">
-            
-            <div className="dt-day-list">
-                {
-                    dates.map((date) => {
-                        const dateValue = date.toLocaleDateString('en-US')
-                        return (
-                            <DateButton
-                                date={date}
-                                onClickButton={() => setSchedule({ ...schedule, selectedDate: dateValue })}
-                            />
-                        )
-                    })
-                }
-
-            </div>
+            {
+                dates.map((date) => {
+                    const dateValue = date.toLocaleDateString('en-US')
+                    return (
+                        <DateButton
+                            selected={dateValue == schedule.selectedDate}
+                            date={date}
+                            onClickButton={() => setSchedule({ ...schedule, selectedDate: dateValue })}
+                        />
+                    )
+                })
+            }
         </div>
     );
 }
