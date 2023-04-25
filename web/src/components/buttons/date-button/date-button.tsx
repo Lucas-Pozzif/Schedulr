0a08eb86d564916f1dc0403f34cb8e34ff00021c
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-04-24 22:23:43 -0300

    date button update

--- web/src/components/buttons/date-button/date-button.tsx
+++ web/src/components/buttons/date-button/date-button.tsx
@@ -1,3 +1,4 @@
+import { isSelected } from '../../../functions/is-selected/is-selected'
 import './date-button.css'
 
 type dateButtonType = {
@@ -7,22 +8,22 @@ type dateButtonType = {
     onClickButton: () => void
 }
 
-export function DateButton({ selected, disabled, date, onClickButton }: dateButtonType) {
+export function DateButton({ selected = false, disabled, date, onClickButton }: dateButtonType) {
     const monthDay = date.toLocaleDateString("pt-BR", { month: "long", day: "numeric" })
     const wd = date.toLocaleDateString("pt-BR", { weekday: 'long' }).split('-')[0]
     const weekday = wd.charAt(0).toUpperCase() + wd.slice(1);
 
     return (
-        <div className="date-button">
-            <p className="dateb-title">{monthDay}</p>
+        <div className={`button date-button ${isSelected(selected)}`} onClick={onClickButton}>
+            <p className={`dateb-title button-text ${isSelected(selected)}`}>{monthDay}</p>
             <div className='datebrb-area'>
                 <div className='datebrb-block'>
-                    <p className="datebrb-side-text">Promocao</p>
-                    <div className="dateb-right-button">
-                        <p className="datebrb-title">{weekday}</p>
+                    <p className={`datebrb-side-text button-text ${isSelected(selected)}`}>Promocao</p>
+                    <div className={`dateb-right-button button ${isSelected(!selected)}`}>
+                        <p className={`datebrb-title button-text ${isSelected(!selected)}`}>{weekday}</p>
                     </div>
                 </div>
-                <p className="datebrb-subtitle">00:00 - 00:00</p>
+                <p className={`datebrb-subtitle button-text ${isSelected(selected)}`}>00:00 - 00:00</p>
             </div>
         </div>
     )
