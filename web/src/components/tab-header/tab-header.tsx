1a15ce2fafcc343bde8b289852fa86b6cc3ff9bc
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-04-27 07:35:52 -0300

    tabheader overall changes

--- web/src/components/tab-header/tab-header.tsx
+++ web/src/components/tab-header/tab-header.tsx
@@ -1,14 +1,22 @@
 import { useNavigate } from "react-router-dom"
-import { scheduleType } from "../../pages/schedule/schedule-add/schedule-add"
+import { scheduleType, selectedServiceType } from "../../pages/schedule/schedule-add/schedule-add"
 import { InviteButton } from "../buttons/invite-button/invite-button"
 import { ReturnButton } from "../buttons/return-button/return-button"
 
 import './tab-header.css'
+import { ScheduleButton } from "../buttons/schedule-button/schedule-button"
 
 type tabHeaderType = {
     scheduleForm: scheduleType
     tab: number,
     setTab: (tab: number) => void,
+    selectedService?: selectedServiceType
+}
+const placeHolderService: selectedServiceType = {
+    service: null,
+    state: 0,
+    professional: null,
+    startTime: null
 }
 
 function scheduleFormValidator(tab: number, scheduleForm: scheduleType): boolean {
@@ -21,20 +29,55 @@ function scheduleFormValidator(tab: number, scheduleForm: scheduleType): boolean
     }
 }
 
-export function TabHeader({ tab, setTab, scheduleForm }: tabHeaderType) {
-    const navigate = useNavigate()
+function headerButtonRender(tab: number, setTab: (tab: number) => void, scheduleForm: scheduleType, selectedService: selectedServiceType = placeHolderService) {
     const titles = [
         "Escolha o melhor dia para você!",
         "Escolha os serviços que interessam.",
-        "Escolha os profissionais para fazer seus serviços com.",
-        "Escolha o melhor horário para cada serviço.",
+        "",
+        "",
         "Verifique os serviços que você agendou"
     ]
     const subtitles = [
         "Estaremos à disposição!",
         "Selecione e continue.",
-        "Atendimento é nossa prioridade!",
-        "Se possível evite sobrepor horários",
+        "",
+        "",
+        "Tudo certo por aqui?"
+    ]
+    switch (tab) {
+        case 0:
+        case 1:
+        case 4:
+            return (
+                <div className="th-title-box">
+                    <p className="th-title">{titles[tab]}</p>
+                    <div className="th-bottom">
+                        <p className="th-subtitle">{subtitles[tab]}</p>
+                        <InviteButton title="Continuar" onClickButton={() => { return tab < 5 ? setTab(tab + 1) : setTab(5) }} />
+                    </div>
+                </div>
+            )
+        case 2:
+        case 3:
+            return <ScheduleButton selectedService={selectedService} onClickButton={() => { setTab(tab + 1) }} rightButtonText="Continuar" />
+    }
+}
+
+export function TabHeader({ tab, setTab, scheduleForm, selectedService }: tabHeaderType) {
+    const navigate = useNavigate()
+    console.log(scheduleForm)
+    const titles = [
+        "Escolha o melhor dia para você!",
+        "Escolha os serviços que interessam.",
+        "",
+        "",
+        "Verifique os serviços que você agendou"
+    ]
+    const subtitles = [
+        "Estaremos à disposição!",
+        "Selecione e continue.",
+        "",
+        "",
         "Tudo certo por aqui?"
     ]
 
@@ -42,13 +85,9 @@ export function TabHeader({ tab, setTab, scheduleForm }: tabHeaderType) {
     return (
         <div className="tab-header">
             <ReturnButton onClickButton={() => { return tab <= 0 ? navigate(-1) : setTab(tab - 1) }} />
-            <div className="th-title-box">
-                <p className="th-title">{titles[tab]}</p>
-                <div className="th-bottom">
-                    <p className="th-subtitle">{subtitles[tab]}</p>
-                    <InviteButton title="Continuar" onClickButton={() => { return tab < 5 ? setTab(tab + 1) : setTab(5) }} />
-                </div>
-            </div>
+            {
+                headerButtonRender(tab, setTab, scheduleForm, selectedService)
+            }
         </div>
     )
 
