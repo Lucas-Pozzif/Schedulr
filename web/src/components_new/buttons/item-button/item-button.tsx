03c055e8b5dd1ddce324883e6512c1a6814d101d
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-05-04 16:23:35 -0300

    item button created

---
+++ web/src/components_new/buttons/item-button/item-button.tsx
@@ -0,0 +1,37 @@
+import { itemButtonType } from "../button-type";
+import { DetailButton2 } from "../detail-button-2/detail-button-2";
+import { DetailButton } from "../detail-button/detail-button";
+import './style.css';
+
+export function ItemButton({
+    title,
+    subtitle,
+    highlightText,
+    detailText,
+    detailSubtitleText,
+    state,
+    onClickButton = () => { },
+    onClickSubtitle = () => { },
+    onClickDetailButton = () => { }
+}: itemButtonType) {
+    const subtitleState = (state == 'active' || state == 'inactive') ? 'selected' : 'active';
+
+    const Subtitle = () => subtitle ? <DetailButton title={subtitle} state={subtitleState} onClickButton={onClickSubtitle} /> : null
+    const Detail = () => subtitle ? <DetailButton2 title={detailText} state={subtitleState} onClickButton={onClickDetailButton} /> : null
+
+    return (
+        <button className={`button item-button ${state}`} onClick={onClickButton}>
+            <div className="item-button-title-block">
+                <p className={`title-button item-button-title ${state}`}>{title}</p>
+                <Subtitle />
+            </div>
+            <div>
+                <div className="flex-div">
+                    <p className="item-button-highlight">{highlightText}</p>
+                    <Detail />
+                </div>
+                <p className="item-button-detail-subtitle-text">{detailSubtitleText}</p>
+            </div>
+        </button>
+    )
+}
