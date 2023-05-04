f0d10839b0ddc284a7ccf68f3be1af413e5d9a78
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-05-04 16:23:27 -0300

    imagebutton created

---
+++ web/src/components_new/buttons/image-button/image-button.tsx
@@ -0,0 +1,41 @@
+import { imageButtonType, itemButtonType } from "../button-type";
+import { DetailButton2 } from "../detail-button-2/detail-button-2";
+import { DetailButton } from "../detail-button/detail-button";
+import './style.css';
+
+export function ImageButton({
+    image,
+    title,
+    subtitle,
+    highlightText,
+    detailText,
+    detailSubtitleText,
+    state,
+    onClickButton = () => { },
+    onClickSubtitle = () => { },
+    onClickDetailButton = () => { }
+}: imageButtonType) {
+    const subtitleState = (state == 'active' || state == 'inactive') ? 'selected' : 'active';
+
+    const Subtitle = () => subtitle ? <DetailButton title={subtitle} state={subtitleState} onClickButton={onClickSubtitle} /> : null
+    const Detail = () => subtitle ? <DetailButton2 title={detailText} state={subtitleState} onClickButton={onClickDetailButton} /> : null
+
+    return (
+        <button className={`button image-button ${state}`} onClick={onClickButton}>
+            <div className="flex-div">
+                <img src={image} className="image-button-image" />
+                <div className="image-button-title-block">
+                    <p className={`title-button image-button-title ${state}`}>{title}</p>
+                    <Subtitle />
+                </div>
+            </div>
+            <div>
+                <div className="flex-div">
+                    <p className="image-button-highlight">{highlightText}</p>
+                    <Detail />
+                </div>
+                <p className="image-button-detail-subtitle-text">{detailSubtitleText}</p>
+            </div>
+        </button>
+    )
+}
