e18516a3683c2f0701ea6958decd27a1b9fc9f09
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-05-05 14:43:31 -0300

    information tab is perfect

--- web/src/pages/professional/professional-form/information-tab/information-tab.tsx
+++ web/src/pages/professional/professional-form/information-tab/information-tab.tsx
@@ -1,18 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 import { Input } from "../../../../components/input/input";
-import { professionalType } from "../../../../controllers/professionalController";
 import updateProfessional from "../../../../functions/updaters/update-professional";
 import { professionalTabType } from "../professional-form";
 import { addOccupation, getAllOccupations, occupationType } from '../../../../controllers/occupationController';
+import { DetailButton } from '../../../../components/buttons/detail-button/detail-button';
+import { SmallButton } from '../../../../components/buttons/small-button/small-button';
+import { VerticalLine } from '../../../../components/line/vertical-line';
+import { IconButton2 } from '../../../../components/buttons/icon-button-2/icon-button-2';
+
+import './style.css'
 
 const occupationCache = require('../../../../cache/occupationCache.json')
 
 export function InformationTab({ professional, setProfessional }: professionalTabType) {
     const [occupationIds, setOccupationIds] = useState<string[] | null>(null)
-    const [occupation, setOccupation] = useState<occupationType>({
-        name: ''
-    })
+    const [occupation, setOccupation] = useState<occupationType>({ name: '' })
 
     useEffect(() => {
         getAllOccupations().then(() => {
@@ -21,7 +24,7 @@ export function InformationTab({ professional, setProfessional }: professionalTa
     }, []);
 
     return (
-        <div>
+        <div className='p-form-information-tab'>
             <Input
                 label="Alterar nome do profissional"
                 value={professional.name}
@@ -32,47 +35,63 @@ export function InformationTab({ professional, setProfessional }: professionalTa
                 label="Alterar email do profissional"
                 value={professional.email}
                 onValueChange={(e) => updateProfessional(professional, setProfessional, 'email', e.target.value)}
-                placeholder="Digite o nome do serviço"
+                placeholder="exemplo@email.com"
             />
-            <p>adicionar lista de ocupações</p>
-            {
-                occupationIds == null ?
-                    <p>loading</p> :
-                    occupationIds!.map((occupationId: string) => {
-                        return (
-                            <div onClick={() => {
-                                let occupations = [...professional.occupations]
 
-                                occupations.includes(occupationId) ?
-                                    occupations = occupations.filter(i => i !== occupationId) :
-                                    occupations.push(occupationId)
+            <div className='flex-div p-form-infotab-bottom'>
+                <div className='p-form-occupation-block'>
+                    <div className='p-form-occupation-list'>
+                        {
+                            occupationIds === null ?
+                                <p>loading</p> :
+                                occupationIds!.map((occupationId: string) =>
+                                    <DetailButton
+                                        state={professional.occupations.includes(occupationId) ? 'selected' : 'active'}
+                                        title={occupationCache[occupationId].name}
+                                        onClickButton={() => {
+                                            let occupations = [...professional.occupations]
 
-                                updateProfessional(professional, setProfessional, 'occupations', occupations)
-                            }} key={occupationId}>
-                                <p>{occupationCache[occupationId].name} {professional.occupations.includes(occupationId) ? '-Selected' : null}</p>
-                            </div>
-                        )
-                    })
-            }
-            <Input
-                label="Não encontrou sua ocupação?"
-                value={occupation.name}
-                onValueChange={(e) => setOccupation({ name: e.target.value })}
-                placeholder="Digite a ocupação"
-            />
-            {
-                occupation.name !== '' ?
-                    <button onClick={async () => {
-                        setOccupationIds(null);
+                                            occupations.includes(occupationId) ?
+                                                occupations = occupations.filter(i => i !== occupationId) :
+                                                occupations.push(occupationId)
 
-                        await addOccupation(occupation)
-                        await getAllOccupations()
+                                            updateProfessional(professional, setProfessional, 'occupations', occupations)
+                                        }}
+                                    />
+                                )
+                        }
 
-                        setOccupation({ name: '' })
-                        setOccupationIds(Object.keys(occupationCache));
+                    </div>
+                    <Input
+                        label="Não encontrou sua ocupação?"
+                        value={occupation.name}
+                        onValueChange={(e) => setOccupation({ name: e.target.value })}
+                        placeholder="Digite a ocupação"
+                    />
+                    <SmallButton
+                        state={occupation.name !== '' ? 'active' : 'inactive'}
+                        title='Adicionar'
+                        onClickButton={async () => {
+                            if (occupation.name === '') return
+                            setOccupationIds(null);
 
-                    }}>Adiconar</button> : null
-            }
+                            await addOccupation(occupation)
+                            await getAllOccupations()
+
+                            setOccupation({ name: '' })
+                            setOccupationIds(Object.keys(occupationCache));
+
+                        }}
+                    />
+                </div>
+                <VerticalLine />
+                <div className='p-form-photo-block'>
+                    <IconButton2 state='inactive' title='Alterar Imagem' icon='a' onClickButton={() => { }} />
+                    <div className='p-form-photo-frame button'>
+                        <img className='p-form-photo' src={professional.photo} />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
