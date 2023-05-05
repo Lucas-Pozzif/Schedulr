1e02c0000fae9ca840499452a039246c8ffa934f
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-05-05 15:13:01 -0300

    ok, NOW it is complete

--- web/src/pages/professional/professional-list/professional-list.tsx
+++ web/src/pages/professional/professional-list/professional-list.tsx
@@ -4,11 +4,15 @@ import { getAllProfessionals } from "../../../controllers/professionalController
 import { Header } from "../../../components/header/header";
 import { ProfessionalButton } from "../../../components/buttons/image-button/professional-button/professional-button";
 
+import './style.css'
+import { Input } from "../../../components/input/input";
+
 const professionalCache = require('../../../cache/professionalCache.json')
 
 function ProfessionalList() {
     const [loading, setLoading] = useState(true);
     const [professionalIds, setProfessionalIds] = useState<string[] | null>(null)
+    const [searchBar, setSearchBar] = useState('')
 
     useEffect(() => {
         getAllProfessionals().then(() => {
@@ -20,7 +24,7 @@ function ProfessionalList() {
     const navigate = useNavigate()
 
     return (
-        <div>
+        <div className="professional-list-page">
             <Header
                 title="Não sabe exatamente quem escolher?"
                 subtitle="Deixa isso com a gente!"
@@ -28,22 +32,27 @@ function ProfessionalList() {
                 onClickButton={() => { navigate('/schedule') }}
                 onClickReturn={() => { navigate(-1) }}
             />
-            {
-                loading ?
-                    <p>loading...</p> :
-                    professionalIds!.map((professionalId: string) => {
-                        const professional = professionalCache[professionalId]
-                        console.log(professional.name)
-                        return (
-                            <ProfessionalButton
-                                state='active'
-                                professional={professional}
-                                detailText="Ver serviços"
-                                onClickButton={() => { }}
-                            />
-                        )
-                    })
-            }
+            <div className="professional-list-block">
+                <Input placeholder="Pesquisar" onValueChange={(e) => { setSearchBar(e.target.value.toLowerCase()) }} />
+                <div className="professional-list">
+                    {
+                        loading ?
+                            <p>loading...</p> :
+                            professionalIds!.map((professionalId: string) => {
+                                const professional = professionalCache[professionalId]
+                                if (!professional.name.toLowerCase().includes(searchBar)) return null
+                                return (
+                                    <ProfessionalButton
+                                        state='active'
+                                        professional={professional}
+                                        detailText="Ver serviços"
+                                        onClickButton={() => { }}
+                                    />
+                                )
+                            })
+                    }
+                </div>
+            </div>
         </div>
     )
 }
