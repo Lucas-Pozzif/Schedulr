301ef8386d2cee2c1759ef4f444fd3068cfc303c
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-05-16 22:14:23 -0300

    storage implementation

--- web/src/firebase/firebase.ts
+++ web/src/firebase/firebase.ts
@@ -1,11 +1,13 @@
 import { initializeApp } from "firebase/app";
 import firebaseConfig from "./firebaseConfig";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth"
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
 const auth = getAuth(app)
+const storage = getStorage();
 
-export { db, auth }
+export { db, auth,storage }
