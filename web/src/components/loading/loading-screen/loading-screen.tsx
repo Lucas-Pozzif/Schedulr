66d67fb3e22b0ecbf757dcfc0a35d6494e09a30c
Lucas Pozzi <74069474+Lucaspozzif@users.noreply.github.com>
2023-05-18 20:58:48 -0300

    loading component creation

---
+++ web/src/components/loading/loading-screen/loading-screen.tsx
@@ -0,0 +1,14 @@
+import './style.css';
+
+export function LoadingScreen() {
+
+    return (
+        <div className="loading-screen">
+            <img 
+            className="loading-logo" 
+            src={"https://firebasestorage.googleapis.com/v0/b/leandro-e-alessandro.appspot.com/o/logo%2Fbrown-light-logo.png?alt=media&token=cdf56dbb-1a4d-4d96-84d1-af033221b736"} 
+            
+            />
+        </div>
+    )
+}
