ae9956ff59d67d11874fb97de80f8f1312cdcb0d
lucas pozzi <lucaspozzif20@gmail.com>
2023-08-28 21:03:35 -0300

    firebase config set up

---
+++ web/src/Services/firebase/firebase-config.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  measurementId: "",
+};
+
+export default firebaseConfig;
