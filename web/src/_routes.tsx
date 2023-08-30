8607002dab6899c815b4d470121fd0a1f18cd389
lucas pozzi <lucaspozzif20@gmail.com>
2023-08-30 13:23:24 -0300

    service form has now a route (ja pode al mossar)

--- web/src/_routes.tsx
+++ web/src/_routes.tsx
@@ -1,7 +1,10 @@
+import { ServiceForm } from "./Forms/service-form/service-form";
 import { Auth } from "./Pages/auth/auth";
 import { Home } from "./Pages/home/home";
 
 export const routes = [
     { path: "/", element: <Home /> },
     { path: "/auth", element: <Auth /> },
+
+    { path: "/service/edit/:serviceId", element: <ServiceForm /> },
 ];
