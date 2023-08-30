c6c47b6cf3407f179920fb9101cc7412366cfc57
lucas pozzi <lucaspozzif20@gmail.com>
2023-08-30 11:12:20 -0300

    home inside the routes

--- web/src/_routes.tsx
+++ web/src/_routes.tsx
@@ -1,6 +1,7 @@
 import { Auth } from "./Pages/auth/auth";
-
+import { Home } from "./Pages/home/home";
 
 export const routes = [
+    { path: "/", element: <Home /> },
     { path: "/auth", element: <Auth /> },
 ];
