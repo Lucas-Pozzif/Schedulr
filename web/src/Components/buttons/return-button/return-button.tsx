3ce107dc42dfe02b3866a8928574116351a94c7a
lucas pozzi <lucaspozzif20@gmail.com>
2023-08-30 11:11:18 -0300

    return button temporary changes for debug

--- web/src/Components/buttons/return-button/return-button.tsx
+++ web/src/Components/buttons/return-button/return-button.tsx
@@ -1,9 +1,6 @@
 import { returnButtonType } from "../button-type";
 import './style.css'
 
-const designCache = require('../../../cache/designCache.json')
-
 export function ReturnButton({ onClickButton }: returnButtonType) {
-    const returnIcon = designCache[0].icons.return
-    return (<img className="return-button" src={returnIcon} onClick={onClickButton} />)
+    return (<img className="return-button" src={''} onClick={onClickButton} />)
 }
