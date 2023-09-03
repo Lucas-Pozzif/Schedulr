15caba198829bd3a1c276e87f067fdd056d424cb
lucas pozzi <lucaspozzif20@gmail.com>
2023-09-03 11:49:59 -0300

    route update

--- web/src/_routes.tsx
+++ web/src/_routes.tsx
@@ -1,11 +1,13 @@
 import { ServiceForm } from "./Forms/service-form/service-form";
 import { Auth } from "./Pages/auth/auth";
 import { Home } from "./Pages/home/home";
+import { ServiceList } from "./Pages/service-list/service-list";
 
 export const routes = [
     { path: "/", element: <Home /> },
     { path: "/auth", element: <Auth /> },
 
+    { path: "/service", element: <ServiceList /> },
     { path: "/service/edit/:serviceId", element: <ServiceForm /> },
     { path: "/service/edit/", element: <ServiceForm /> },
 ];
