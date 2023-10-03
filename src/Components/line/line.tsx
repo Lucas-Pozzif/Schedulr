aa8cbf455fe549917cb2e5b8335b8376653f6116
lucas pozzi <lucaspozzif20@gmail.com>
2023-10-02 21:53:55 -0300

    line component

---
+++ src/Components/line/line.tsx
@@ -0,0 +1,13 @@
+import './line.css'
+
+export function Line() {
+    return (
+        <div className="line"></div>
+    )
+}
+
+export function VerticalLine() {
+    return (
+        <div className="vertical-line"></div>
+    )
+}
