6f0e0979f505470e1a083b49861b309d25fded6e
lucas pozzi <lucaspozzif20@gmail.com>
2023-10-06 17:08:09 -0300

    service form half done

--- src/Forms/service-form/service-form.tsx
+++ src/Forms/service-form/service-form.tsx
@@ -3,6 +3,8 @@ import { Service } from "../../Classes/service";
 import { User } from "../../Classes/user";
 import { LoadingScreen } from "../../Components/loading/loading-screen/loading-screen";
 import { Line } from "../../Components/line/line";
+import { useNavigate } from "react-router-dom";
+import { Header } from "../../Components/header/header";
 
 type ServiceFormType = {
     user?: User
@@ -12,10 +14,14 @@ export function ServiceForm({ user, service = new Service() }: ServiceFormType) 
     const [loading, setLoading] = useState(false);
     const [serviceForm, setServiceForm] = useState(service);
     const [tab, setTab] = useState(0)
+    const [selectedSService, setSelectedSService] = useState<null | string>(null)
+
+    const navigate = useNavigate()
 
     const arrow = require("../../Assets/arrow.png");
     const bin = require("../../Assets/delete.png");
     const money = require("../../Assets/money.png");
+    const addImage = require("../../Assets/add-image.png");
 
     const tabHandler = () => {
         switch (tab) {
@@ -23,7 +29,7 @@ export function ServiceForm({ user, service = new Service() }: ServiceFormType) 
                 return (
                     <div className="service-form">
                         <div className="sf-header">
-                            <img className="return-button" src={arrow} />
+                            <img className="return-button" src={arrow} onClick={() => { navigate(-1) }} />
                             <div className="sf-header-text-block">
                                 <input className="sf-header-title" placeholder="Nome do Serviço" value={serviceForm.getName()} onChange={(e) => {
                                     serviceForm.setName(e.target.value);
@@ -32,6 +38,7 @@ export function ServiceForm({ user, service = new Service() }: ServiceFormType) 
                                 }} />
                                 <p className="sf-header-subtitle"></p>
                             </div>
+                            <img className="sf-delete-button" src={bin} onClick={() => { alert("ainda não implementado") }} />
                         </div>
                         <div className="sf-value-block">
                             <img className="sf-value-icon" src={money} />
@@ -44,14 +51,36 @@ export function ServiceForm({ user, service = new Service() }: ServiceFormType) 
                         <Line />
                         <div className="sf-bottom-columns">
                             <div className="sf-left-column">
-
+                                <p className={"sf-inicial-selector" + (serviceForm.getInicial() ? " selected" : "")} onClick={() => {
+                                    serviceForm.setInicial(!serviceForm.getInicial)
+                                    const updatedService = new Service(serviceForm)
+                                    setServiceForm(updatedService)
+                                }}>A partir de</p>
+                                <div className="sf-subservice-selector">
+                                    <p className="sf-subservice-title">{ }</p>
+                                    <img className="sched-arrow up" src={arrow} />
+                                </div>
                             </div>
                             <div className="sf-right-column">
-
+                                <div className="sf-selector" onClick={() => { setTab(1) }}>
+                                    <p className="sf-selector-text">Alterar tempo de duração</p>
+                                    <img className={"sched-arrow right"} src={arrow} />
+                                </div>
+                                <div className="sf-selector" onClick={() => { setTab(2) }}>
+                                    <p className="sf-selector-text">Alterar profissionais</p>
+                                    <img className={"sched-arrow right"} src={arrow} />
+                                </div>
+                            </div>
+                            <div className="sf-image-group">
+                                <div className="sf-image-add">
+                                    <img className="sf-image-add-icon" src={addImage} />
+                                </div>
+                                {serviceForm.getPhotos().map((image) => { return (<img className="gf-image" src={image} />) })}
                             </div>
                         </div>
                     </div>
                 )
+            case 1:
             default: return <div />
         }
     }
