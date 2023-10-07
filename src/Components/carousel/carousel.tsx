702139c2c4cde69d661aed843765a2b10a21355f
lucas pozzi <lucaspozzif20@gmail.com>
2023-10-06 21:06:39 -0300

    carousel component

---
+++ src/Components/carousel/carousel.tsx
@@ -0,0 +1,21 @@
+
+import "./carousel.css"
+
+type carouselType = {
+    items: {
+        title: string;
+        isSelected: boolean;
+        onClick: () => void
+    }[];
+}
+
+export function Carousel({ items }: carouselType) {
+    return (
+        <div className="carousel">
+            {
+                items.map((item) => {
+                    return (<p className={"carousel-item" + (item.isSelected ? " selected" : "")} onClick={item.onClick}>{item.title}</p>)
+                })
+            }
+        </div>)
+}
