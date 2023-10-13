141ff2094956cec90e73384ed0286db34b6b29ab
lucas pozzi <lucaspozzif20@gmail.com>
2023-10-13 17:12:36 -0300

    app useless code remvoed

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,23 @@
-import React, { useEffect, useState } from "react";
-import { User } from "./Classes/user";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { onAuthStateChanged, User as FirebaseUser } from "firebase/auth";
-import { auth } from "./Services/firebase/firebase";
-import { LoadingScreen } from "./Components/loading/loading-screen/loading-screen";
 import { routes } from "./_routes";
-
 
 import './Styles/reset.css'
 import './Styles/style.css'
 
 export function App() {
-  const [loading, setLoading] = useState(false);
-  const [user, setUser] = useState(new User());
-
-  useEffect(() => {
-    setLoading(true);
-    onAuthStateChanged(auth, async (client: FirebaseUser | null) => {
-      if (!client) return setLoading(false);
-      //await user.getUser(client.uid);
-      console.log('user updated')
-      setUser(user); // Update the user state with the fetched user data
-      setLoading(false);
-    });
-  }, []);
-
-  return loading ? (
-    <LoadingScreen />
-  ) : (
+  return (
     <BrowserRouter>
       <Routes>
-        {routes.map((route: any) => {
-          return (
-            <Route
-              path={route.path}
-              element={React.cloneElement(route.element, { user: user })}
-            ></Route>
-          );
-        })}
+        {
+          routes.map((route: any) => {
+            return (
+              <Route
+                path={route.path}
+                element={route.element}
+              />
+            );
+          })
+        }
       </Routes>
     </BrowserRouter>
   );
