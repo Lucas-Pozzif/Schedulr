f5b7e6f5d79acba17ce6526f537cf4768a683730
lucas pozzi <lucaspozzif20@gmail.com>
2023-10-13 17:12:03 -0300

    home page user and group implementation

--- src/Pages/home/home.tsx
+++ src/Pages/home/home.tsx
@@ -3,23 +3,46 @@ import { Group } from "../../Classes/group";
 import { GroupList } from "../../Components/group-list/group-list";
 import { VerticalLine } from "../../Components/line/line";
 import { User } from "../../Classes/user";
+import { useNavigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../Services/firebase/firebase";
+import { LoadingScreen } from "../../Components/loading/loading-screen/loading-screen";
 
 import "./home.css";
-import { useNavigate } from "react-router-dom";
 
 export function Home() {
+    const [loading, setLoading] = useState(false);
     const [user, setUser] = useState(new User())
+    const [groups, setGroups] = useState<any[]>([])
 
     const navigate = useNavigate()
 
-    const group = new Group()
-    group.getGroup("2")
 
     const search = require('../../Assets/search.png');
     const filter = require('../../Assets/filter.png');
     const sort = require('../../Assets/sort.png');
 
-    return (
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            onAuthStateChanged(auth, async (client) => {
+                if (client) {
+                    await user.getUser(client.uid);
+                    setUser(new User(user));
+                }
+            });
+            const groupList = await user.getGroups()
+            setGroups(groupList)
+            setLoading(false);
+        };
+
+        fetchData();
+    }, [])
+
+    console.log(groups)
+
+    return loading ?
+        <LoadingScreen /> :
         <div className="home">
             <div className="h-header">
                 <p className="h-title">Boa Tarde {user.getName()}</p>
@@ -30,7 +53,7 @@ export function Home() {
                     <VerticalLine />
                     <img className="h-filter-icon" src={filter} />
                 </div>
-                <img className="h-user-icon" src={""} onClick={() => { navigate("/user") }} />
+                <img className="h-user-icon" src={user.getPhoto()} onClick={() => { navigate("/user") }} />
             </div>
             <div className="h-banner-block">
                 <div className="h-banner">
@@ -40,7 +63,6 @@ export function Home() {
                     <img className="hb-sort-icon" src={sort} />
                 </div>
             </div>
-            <GroupList groupList={[group, group, group, group, group, group]} />
+            <GroupList groupList={groups} />
         </div>
-    )
 }
