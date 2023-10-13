9e2f1824c5ea108b8472e7dd6f60b6427b95f5b4
lucas pozzi <lucaspozzif20@gmail.com>
2023-10-13 17:12:43 -0300

    new paths

--- src/_routes.tsx
+++ src/_routes.tsx
@@ -1,7 +1,11 @@
+import { GroupForm } from "./Forms/group-form/group-form";
+import { GroupPage } from "./Pages/group-page/group-page";
 import { Home } from "./Pages/home/home";
 import { UserPage } from "./Pages/user-page/user-page";
 
 export const routes = [
     { path: "/", element: <Home /> },
     { path: "/user", element: <UserPage /> },
+    { path: "/group/:groupId", element: <GroupPage /> },
+    { path: "/group/add", element: <GroupForm /> },
 ];
