c8820d9d6bd0b5270c45ce9c0bc04b753067b3b5
lucas pozzi <lucaspozzif20@gmail.com>
2023-10-27 20:39:38 -0300

    new route

--- src/_routes.tsx
+++ src/_routes.tsx
@@ -1,13 +1,14 @@
 import { GroupForm } from "./Forms/group-form/group-form";
-import { ProfessionalForm } from "./Forms/professional-form/professional-form";
 import { GroupPage } from "./Pages/group-page/group-page";
 import { Home } from "./Pages/home/home";
+import { ClientSchedulePage } from "./Pages/schedule-page/client-schedule/client-schedule-page";
 import { ProfessionalSchedulePage } from "./Pages/schedule-page/professional-schedule/professional-schedule-page";
 import { UserPage } from "./Pages/user-page/user-page";
 
 export const routes = [
   { path: "/", element: <Home /> },
   { path: "/user", element: <UserPage /> },
+  { path: "/user/schedule/:userId", element: <ClientSchedulePage /> },
   { path: "/group/:groupId", element: <GroupPage /> },
   { path: "/group/add", element: <GroupForm /> },
   { path: "/professional/schedule/:professionalId", element: <ProfessionalSchedulePage /> },
