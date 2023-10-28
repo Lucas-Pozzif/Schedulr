8c05032adaa66b821960c50b89bc7ab6e4bcdfb6
lucas pozzi <lucaspozzif20@gmail.com>
2023-10-28 12:43:47 -0300

    header formatted by prettier

--- src/Components/header/header.tsx
+++ src/Components/header/header.tsx
@@ -1,20 +1,20 @@
-import './header.css'
+import "./header.css";
 type headerType = {
-    title: string
-    icon: string
-    onClickReturn: () => void
-    onClickIcon: () => void
-}
+  title: string;
+  icon: string;
+  onClickReturn: () => void;
+  onClickIcon: () => void;
+};
 
 export function Header({ title, icon, onClickReturn, onClickIcon }: headerType) {
+  const arrow = require("../../Assets/arrow.png");
+  const rectangle = require("../../Assets/rectangle.png");
 
-    const arrow = require("../../Assets/arrow.png");
-
-    return (
-        <div className="header">
-            <img className="return-button" src={arrow} onClick={onClickReturn} />
-            <p className="gf-header-title">{title}</p>
-            <img className="header-icon" src={icon} onClick={onClickIcon} />
-        </div>
-    )
+  return (
+    <div className='header'>
+      <img className='return-button' src={arrow} onClick={onClickReturn} />
+      <p className='gf-header-title'>{title}</p>
+      <img className='header-icon' src={icon === "" ? rectangle : icon} onClick={onClickIcon} />
+    </div>
+  );
 }
