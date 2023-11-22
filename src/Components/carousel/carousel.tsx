788aeb8054e4d0cc7c75b8d10c17077071634acd
lucas pozzi <lucaspozzif20@gmail.com>
2023-11-22 18:54:45 -0300

    carousel formatted

--- src/Components/carousel/carousel.tsx
+++ src/Components/carousel/carousel.tsx
@@ -1,21 +1,23 @@
-
-import "./carousel.css"
+import "./carousel.css";
 
 type carouselType = {
-    items: {
-        title: string;
-        isSelected: boolean;
-        onClick: () => void
-    }[];
-}
+  items: {
+    title?: string;
+    isSelected?: boolean;
+    onClick?: () => void;
+  }[];
+};
 
 export function Carousel({ items }: carouselType) {
-    return (
-        <div className="carousel">
-            {
-                items.map((item) => {
-                    return (<p className={"carousel-item" + (item.isSelected ? " selected" : "")} onClick={item.onClick}>{item.title}</p>)
-                })
-            }
-        </div>)
+  return (
+    <div className='carousel'>
+      {items.map((item) => {
+        return (
+          <p className={"carousel-item" + (item.isSelected ? " selected" : "")} onClick={item.onClick}>
+            {item.title}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
