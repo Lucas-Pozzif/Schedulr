4daaff68d622d58ee63bb23eede191f791a2968b
lucas pozzi <lucaspozzif20@gmail.com>
2023-11-22 18:54:01 -0300

    double button standardized

--- src/Components/buttons/bottom-popup/bottom-popup.tsx
+++ src/Components/buttons/bottom-popup/bottom-popup.tsx
@@ -1,21 +1,23 @@
-import './bottom-popup.css'
+import "./bottom-popup.css";
 
 type bottomPopupType = {
-    title: string;
-    subtitle: string;
-    buttonTitle: string;
-    onClick: () => void
-    isActive: boolean
-}
+  title: string;
+  subtitle: string;
+  buttonTitle: string;
+  onClick: () => void;
+  activated: boolean;
+};
 
-export function BottomPopup({ title, subtitle, buttonTitle, onClick, isActive }: bottomPopupType) {
-    return (
-        <div className={"bottom-popup"}>
-            <div className="bp-text-block">
-                <p className="bp-title">{title}</p>
-                <p className="bp-subtitle">{subtitle}</p>
-            </div>
-            <p className={"bp-button" + (isActive ? "" : " inactive")} onClick={() => isActive ? onClick() : null}>{buttonTitle}</p>
-        </div>
-    )
+export function BottomPopup({ title, subtitle, buttonTitle, onClick, activated }: bottomPopupType) {
+  return (
+    <div className={"bottom-popup"}>
+      <div className='bp-text-block'>
+        <p className='bp-title'>{title}</p>
+        <p className='bp-subtitle'>{subtitle}</p>
+      </div>
+      <p className={"bp-button" + (activated ? "" : " inactive")} onClick={() => (activated ? onClick() : null)}>
+        {buttonTitle}
+      </p>
+    </div>
+  );
 }
