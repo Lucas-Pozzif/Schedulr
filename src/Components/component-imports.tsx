f1b125d35933422faffcd6500a9ce400d2d9b167
lucas pozzi <lucaspozzif20@gmail.com>
2023-11-23 19:30:22 -0300

    SCHED-9 components now have a standard import file

---
+++ src/Components/component-imports.tsx
@@ -0,0 +1,14 @@
+import { LoadingScreen } from "./loading/loading-screen/loading-screen";
+import { Line } from "./line/line";
+import { Header } from "./header/header/header";
+import { LinkButton } from "./buttons/link-button/link-button";
+import { BottomPopup } from "./buttons/bottom-popup/bottom-popup";
+import { BottomButton } from "./buttons/bottom-button/bottom-button";
+import { SubHeader } from "./sub-header/sub-header";
+import { ItemButton } from "./buttons/item-button/item-button";
+import { Carousel } from "./carousel/carousel";
+import { GroupBanner } from "./banner/group-banner/group-banner";
+import { DoubleInput } from "./inputs/double-input/double-input";
+import { IconInput } from "./inputs/icon-input/icon-input";
+
+export { LoadingScreen, Line, Header, LinkButton, BottomPopup, BottomButton, SubHeader, ItemButton, Carousel, GroupBanner, DoubleInput, IconInput };
