b8d350f89b573430daf2723a8df1cfcb0ea92203
lucas pozzi <lucaspozzif20@gmail.com>
2023-11-23 19:30:05 -0300

    SCHED-9 classes now have a default import

---
+++ src/classes/classes-imports.tsx
@@ -0,0 +1,7 @@
+import { Group } from "./group";
+import { Professional } from "./professional";
+import type { Schedule } from "./schedule";
+import { Service } from "./service";
+import { User } from "./user";
+
+export { Group, Professional, Schedule, Service, User };
