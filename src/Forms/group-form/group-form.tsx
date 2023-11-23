2e946832bd5e111778f6c14b7b88bebc48d8c8a1
lucas pozzi <lucaspozzif20@gmail.com>
2023-11-23 19:30:59 -0300

    SCHED-9 code review and optimization of groupform

--- src/Forms/group-form/group-form.tsx
+++ src/Forms/group-form/group-form.tsx
@@ -1,52 +1,36 @@
-import { useEffect, useRef, useState } from "react";
-
-import { Group } from "../../Classes/group";
-import { User } from "../../Classes/user";
-import { LoadingScreen } from "../../Components/loading/loading-screen/loading-screen";
-import { Line } from "../../Components/line/line";
-import { Service } from "../../Classes/service";
-import { Professional } from "../../Classes/professional";
-import { Header } from "../../Components/header/header/header";
-import { LinkButton } from "../../Components/buttons/link-button/link-button";
-import { BottomPopup } from "../../Components/buttons/bottom-popup/bottom-popup";
-import { BottomButton } from "../../Components/buttons/bottom-button/bottom-button";
-import { SubHeader } from "../../Components/sub-header/sub-header";
-import { ItemButton } from "../../Components/buttons/item-button/item-button";
-import { ServiceForm } from "../service-form/service-form";
-import { Carousel } from "../../Components/carousel/carousel";
-import { ProfessionalForm } from "../professional-form/professional-form";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../../Services/firebase/firebase";
-import { useNavigate } from "react-router-dom";
-
 import "./group-form.css";
-import { GroupBanner } from "../../Components/banner/group-banner/group-banner";
-import { DoubleInput } from "../../Components/inputs/double-input/double-input";
-import { IconInput } from "../../Components/inputs/icon-input/icon-input";
-import ErrorPage from "../../Pages/error-page/error-page";
 
-export function GroupForm() {
+import { useEffect, useRef, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../Services/firebase/firebase";
+import { addImage, addUser, block, fullDays, locationPin, more, timeArray } from "../../_global";
+import { LoadingScreen, Line, Header, LinkButton, BottomPopup, BottomButton, SubHeader, ItemButton, Carousel, GroupBanner, DoubleInput, IconInput } from "../../Components/component-imports";
+import { Group, Professional, Service, User } from "../../Classes/classes-imports";
+import { handleImageInput, onClickRef, stateSwitcher } from "../../Function/functions-imports";
+
+import { ServiceForm } from "../service-form/service-form";
+import { ProfessionalForm } from "../professional-form/professional-form";
+import { ErrorPage } from "../../Pages/error-page/error-page";
+
+type GroupFormType = {
+  group?: Group;
+  onClickReturn?: () => void;
+};
+
+export function GroupForm({ group, onClickReturn }: GroupFormType) {
   const [user, setUser] = useState(new User());
   const [loading, setLoading] = useState(false);
-  const [groupForm, setGroupForm] = useState(new Group());
-  const [GTShow, setGTShow] = useState(false); //Group type
+  const [groupForm, setGroupForm] = useState(new Group(group));
   const [tab, setTab] = useState(0);
-  const [dayList, setDayList] = useState(false);
-  const [selectedDay, setSelectedDay] = useState(1);
-  const [selectedService, setSelectedService] = useState<null | string>(null);
-  const [selectedProfessional, setSelectedProfessional] = useState<null | string>(null);
 
-  const days = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
-  const fullDays = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
-
-  const addImage = require("../../Assets/add-image.png");
-  const locationPin = require("../../Assets/location-pin.png");
-  const arrow = require("../../Assets/arrow.png");
-  const more = require("../../Assets/more.png");
-  const addUser = require("../../Assets/add-user.png");
-  const block = require("../../Assets/block.png");
+  const [selectedDay, setSelectedDay] = useState(1); // Weekday
+  const [selectedService, setSelectedService] = useState<null | string>(null); // Selected Service
+  const [selectedProfessional, setSelectedProfessional] = useState<null | string>(null); // Selected Professional
 
   const navigate = useNavigate();
+  const bannerRef = useRef<HTMLInputElement>(null);
+  const profileRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     setLoading(true);
@@ -59,68 +43,42 @@ export function GroupForm() {
     groupForm.setAdmins([...groupForm.getAdmins(), user.getId()]);
     setLoading(false);
   }, []);
-  const bannerRef = useRef<HTMLInputElement>(null);
-  const profileRef = useRef<HTMLInputElement>(null);
+
+  const saveGroupForm = async (group: Group, setLoading: React.Dispatch<React.SetStateAction<boolean>>) => {
+    setLoading(true);
+    if (group.getId()) await group.setGroup();
+    else await group.addGroup();
+    setLoading(false);
+  };
 
   const tabHandler = () => {
     switch (tab) {
       case 0: // Home tab
         return (
           <div className='group-form'>
-            <GroupBanner onClick={() => bannerRef.current!.click()} banner={groupForm.getBanner()} />
-            <div className='gf-profile' onClick={() => profileRef.current!.click()}>
+            <GroupBanner onClick={() => onClickRef(bannerRef)} banner={groupForm.getBanner()} />
+            <div className='gf-profile' onClick={() => onClickRef(profileRef)}>
               <img className={`gf-profile-${groupForm.getProfile() ? "image" : "placeholder"}`} src={groupForm.getProfile() ? groupForm.getProfile() : addImage} />
             </div>
             <DoubleInput
               input1={{
                 placeholder: "Editar nome",
                 value: groupForm.getTitle(),
-                onChange: (e) => {
-                  groupForm.setTitle(e.target.value);
-                  const updatedGroup = new Group(groupForm);
-                  setGroupForm(updatedGroup);
-                },
+                onChange: (e) => groupForm.updateGroupState(setGroupForm, "title", e.target.value),
               }}
               input2={{
                 placeholder: "Editar tipo de negócio",
                 value: groupForm.getType(),
-                onChange: (e) => {
-                  groupForm.setType(e.target.value);
-                  setGroupForm(new Group(groupForm));
-                },
+                onChange: (e) => groupForm.updateGroupState(setGroupForm, "type", e.target.value),
               }}
             />
             <div className='gf-data-block'>
-              <IconInput
-                icon={locationPin}
-                value={groupForm.getLocation()}
-                placeholder='Digite o Endereço'
-                onChange={(e) => {
-                  groupForm.setLocation(e.target.value);
-                  const updatedGroup = new Group(groupForm);
-                  setGroupForm(updatedGroup);
-                }}
-              />
+              <IconInput icon={locationPin} value={groupForm.getLocation()} placeholder='Digite o Endereço' onChange={(e) => groupForm.updateGroupState(setGroupForm, "location", e.target.value)} />
               <Line />
               <div className='gf-links'>
-                <LinkButton
-                  title='Editar Serviços'
-                  onClick={() => {
-                    setTab(1);
-                  }}
-                />
-                <LinkButton
-                  title='Editar Horários'
-                  onClick={() => {
-                    setTab(2);
-                  }}
-                />
-                <LinkButton
-                  title='Editar Profissionais'
-                  onClick={() => {
-                    setTab(3);
-                  }}
-                />
+                <LinkButton title='Editar Serviços' onClick={() => setTab(1)} />
+                <LinkButton title='Editar Horários' onClick={() => setTab(2)} />
+                <LinkButton title='Editar Profissionais' onClick={() => setTab(3)} />
               </div>
             </div>
             <BottomPopup
@@ -128,46 +86,14 @@ export function GroupForm() {
               subtitle='Possui Alterações'
               buttonTitle='Salvar Alterações'
               onClick={async () => {
-                setLoading(true);
-                if (groupForm.getId()) {
-                  await groupForm.setGroup();
-                } else {
-                  await groupForm.addGroup();
-                }
-                console.log("Grupos atualizados");
-
-                setLoading(false);
+                await saveGroupForm(groupForm, setLoading);
                 navigate("/");
               }}
               activated={groupForm.isValid()}
             />
             {/* Hidden inputs that are refferencied */}
-            <input
-              className='hidden'
-              type='file'
-              accept='image/*'
-              onChange={(event) => {
-                const selectedFile = event.target.files?.[0];
-                if (selectedFile) {
-                  groupForm.setBanner(URL.createObjectURL(selectedFile));
-                  setGroupForm(new Group(groupForm));
-                }
-              }}
-              ref={bannerRef}
-            />
-            <input
-              className='hidden'
-              type='file'
-              accept='image/*'
-              onChange={(event) => {
-                const selectedFile = event.target.files?.[0];
-                if (selectedFile) {
-                  groupForm.setProfile(URL.createObjectURL(selectedFile));
-                  setGroupForm(new Group(groupForm));
-                }
-              }}
-              ref={profileRef}
-            />
+            <input className='hidden' type='file' accept='image/*' onChange={(event) => handleImageInput(event, groupForm, setGroupForm, "banner")} ref={bannerRef} />
+            <input className='hidden' type='file' accept='image/*' onChange={(event) => handleImageInput(event, groupForm, setGroupForm, "profile")} ref={profileRef} />
           </div>
         );
       case 1: // Service Tab
@@ -180,9 +106,7 @@ export function GroupForm() {
                 setTab(0);
                 setSelectedService(null);
               }}
-              onClickIcon={() => {
-                setTab(4);
-              }}
+              onClickIcon={() => setTab(4)}
             />
             <SubHeader
               title={`${groupForm.getServices().length} serviços criados`}
@@ -197,140 +121,31 @@ export function GroupForm() {
                 .getServices()
                 .sort((a, b) => a.getName().localeCompare(b.getName()))
                 .map((service: Service) => {
-                  return (
-                    <ItemButton
-                      title={service.getName()}
-                      subtitle={service.getDurationValue()}
-                      selected={selectedService === service.getId()}
-                      onClick={() => {
-                        if (selectedService == service.getId()) {
-                          setSelectedService(null);
-                        } else {
-                          setSelectedService(service.getId());
-                        }
-                      }}
-                    />
-                  );
+                  return <ItemButton title={service.getName()} subtitle={service.getDurationValue()} selected={selectedService === service.getId()} onClick={() => stateSwitcher(selectedService, service.getId(), setSelectedService)} />;
                 })}
             </div>
-            <BottomButton
-              hidden={selectedService == null}
-              title='Editar Serviço'
-              onClick={() => {
-                setTab(4);
-              }}
-            />
+            <BottomButton hidden={selectedService == null} title='Editar Serviço' onClick={() => setTab(4)} />
           </div>
         );
       case 2: // Time Tab
-        const timeArray = [];
-
-        for (let i = 0; i <= 24; i++) {
-          timeArray.push(`${i}:00`, `${i}:30`);
-        }
         return (
           <div className='gf-tab'>
-            <Header
-              title='Alterar Horários'
-              icon={block}
-              onClickIcon={() => {
-                const startHours = [...groupForm.getStartHours()];
-                const hours = [...groupForm.getHours()];
-                startHours[selectedDay] = 0;
-                hours[selectedDay] = [];
-                groupForm.setStartHours(startHours);
-                groupForm.setHours([]);
-                const updatedGroupForm = new Group(groupForm);
-                setGroupForm(updatedGroupForm);
-              }}
-              onClickReturn={() => {
-                setTab(1);
-              }}
-            />
-            <SubHeader
-              title={fullDays[selectedDay]}
-              buttonTitle='Preencher horários'
-              onClick={() => {
-                const hours = [...groupForm.getHours()];
-                hours[selectedDay] = hours[selectedDay]?.map(() => true);
-
-                groupForm.setHours(hours);
-                setGroupForm(new Group(groupForm));
-              }}
-            />
+            <Header title='Alterar Horários' icon={block} onClickIcon={() => groupForm.cleanDay(selectedDay, setGroupForm)} onClickReturn={() => setTab(1)} />
+            <SubHeader title={fullDays[selectedDay]} buttonTitle='Preencher horários' onClick={() => groupForm.fillHours(selectedDay, setGroupForm)} />
             <Carousel
-              items={fullDays.map((day, index) => {
-                return {
-                  title: day,
-                  selected: selectedDay == index,
-                  onClick: () => {
-                    setSelectedDay(index);
-                  },
-                };
-              })}
+              items={fullDays.map((day, index) => ({
+                title: day,
+                selected: selectedDay === index,
+                onClick: () => setSelectedDay(index),
+              }))}
             />
             <div className='gf-list'>
               {timeArray.map((timeValue, index) => {
-                const startHour = [...groupForm.getStartHours()];
-                const hours = [...groupForm.getHours()];
                 const selected = groupForm.getHours()[selectedDay]?.[index - groupForm.getStartHours()[selectedDay]];
-                if (index < 12) {
-                  return;
-                }
-                return (
-                  <ItemButton
-                    title={timeValue}
-                    subtitle={""}
-                    selected={selected}
-                    onClick={() => {
-                      if (!startHour[selectedDay]) {
-                        startHour[selectedDay] = 0;
-                      }
-                      startHour[selectedDay] = parseInt(startHour[selectedDay].toString());
-
-                      if (isNaN(startHour[selectedDay])) {
-                        startHour[selectedDay] = 0;
-                      }
-
-                      if (startHour[selectedDay] > 0) {
-                        const falseValuesToAdd = Array(startHour[selectedDay]).fill(false);
-                        hours[selectedDay] = [...falseValuesToAdd, ...hours[selectedDay]];
-                        startHour[selectedDay] = 0;
-                      }
-                      if (!hours[selectedDay]) {
-                        hours[selectedDay] = [];
-                      }
-
-                      if (index >= hours[selectedDay].length) {
-                        const diff = index - hours[selectedDay].length + 1;
-                        hours[selectedDay].push(...Array(diff).fill(false));
-                      }
-                      hours[selectedDay][index] = !hours[selectedDay][index];
-                      let lastIndex = hours[selectedDay].length - 1;
-                      while (lastIndex >= 0 && !hours[selectedDay][lastIndex]) {
-                        hours[selectedDay].pop();
-                        lastIndex--;
-                      }
-                      startHour[selectedDay] = hours[selectedDay].indexOf(true);
-                      for (let i = 0; i < startHour[selectedDay]; i++) {
-                        hours[selectedDay].shift();
-                      }
-                      groupForm.setHours(hours);
-                      groupForm.setStartHours(startHour);
-                      const updatedGroupForm = new Group(groupForm);
-                      setGroupForm(updatedGroupForm);
-                    }}
-                  />
-                );
+                return index >= 12 ? <ItemButton title={timeValue} subtitle={""} selected={selected} onClick={() => groupForm.updateHourList(selectedDay, index, setGroupForm)} /> : null;
               })}
             </div>
-            <BottomButton
-              hidden={false}
-              title={"Confirmar"}
-              onClick={() => {
-                setTab(0);
-              }}
-            />
+            <BottomButton hidden={false} title={"Confirmar"} onClick={() => setTab(0)} />
           </div>
         );
       case 3: // Professional Tab
@@ -343,34 +158,17 @@ export function GroupForm() {
                 setTab(0);
                 setSelectedProfessional(null);
               }}
-              onClickIcon={() => {
-                setTab(5);
-              }}
+              onClickIcon={() => setTab(5)}
             />
             <div className='gf-professional-list'>
               {groupForm
                 .getProfessionals()
                 .sort((a, b) => a.getName().localeCompare(b.getName()))
                 .map((professional: Professional, index: number) => {
-                  return (
-                    <ItemButton
-                      title={professional.getName()}
-                      subtitle={professional.getOccupations().join(", ")}
-                      selected={selectedProfessional === professional.getId()}
-                      onClick={() => {
-                        setSelectedProfessional(professional.getId());
-                      }}
-                    />
-                  );
+                  return <ItemButton title={professional.getName()} subtitle={professional.getOccupations().join(", ")} selected={selectedProfessional === professional.getId()} onClick={() => setSelectedProfessional(professional.getId())} />;
                 })}
             </div>
-            <BottomButton
-              hidden={selectedProfessional == null}
-              title={"Editar Profissional"}
-              onClick={() => {
-                setTab(5);
-              }}
-            />
+            <BottomButton hidden={selectedProfessional == null} title={"Editar Profissional"} onClick={() => setTab(5)} />
           </div>
         );
       case 4: // ServiceForm Tab
@@ -378,17 +176,7 @@ export function GroupForm() {
         const service = services.find((service) => {
           return service.getId() == selectedService;
         });
-        return (
-          <ServiceForm
-            user={user}
-            groupForm={groupForm}
-            setGroupForm={setGroupForm}
-            service={service}
-            onClickReturn={() => {
-              setTab(1);
-            }}
-          />
-        );
+        return <ServiceForm user={user} groupForm={groupForm} setGroupForm={setGroupForm} service={service} onClickReturn={() => setTab(1)} />;
       case 5: // ProfessionalForm Tab
         const professionals = groupForm.getProfessionals();
         const professional = professionals.find((professional) => {
@@ -399,6 +187,5 @@ export function GroupForm() {
         return <ErrorPage />;
     }
   };
-
   return loading ? <LoadingScreen /> : tabHandler();
 }
