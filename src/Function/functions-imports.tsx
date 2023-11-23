474b0c9db4ffb66ce11ea6dc8492dce03e47095a
lucas pozzi <lucaspozzif20@gmail.com>
2023-11-23 19:28:13 -0300

    SCHED-9  function import is now standardized

---
+++ src/Function/functions-imports.tsx
@@ -0,0 +1,4 @@
+import { handleImageInput, onClickRef } from "./click-handlers/on-click-ref";
+import { stateSwitcher } from "./stateSwitcher/stateSwitcher";
+
+export { handleImageInput, onClickRef, stateSwitcher };
