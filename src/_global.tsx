2bc09a2714c389897c42afbbdb753089b4342302
lucas pozzi <lucaspozzif20@gmail.com>
2023-11-23 19:26:05 -0300

    SCHED-9 global variables location global variables now have their own space

---
+++ src/_global.tsx
@@ -0,0 +1,12 @@
+export const timeArray: string[] = [];
+
+for (let i = 0; i <= 24; i++) {
+  timeArray.push(`${i}:00`, `${i}:30`);
+}
+export const fullDays = ["Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado", "Domingo"];
+
+export const addImage = require("./Assets/add-image.png");
+export const locationPin = require("./Assets/location-pin.png");
+export const more = require("./Assets/more.png");
+export const addUser = require("./Assets/add-user.png");
+export const block = require("./Assets/block.png");
