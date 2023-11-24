6dc18cdd60a0b41246533ff6924bfde7c03b7090
lucas pozzi <lucaspozzif20@gmail.com>
2023-11-24 11:17:00 -0300

    SCHED-9 new global variables

--- src/_global.tsx
+++ src/_global.tsx
@@ -3,10 +3,12 @@ export const timeArray: string[] = [];
 for (let i = 0; i <= 24; i++) {
   timeArray.push(`${i}:00`, `${i}:30`);
 }
-export const fullDays = ["Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado", "Domingo"];
+export const fullDays = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
 
 export const addImage = require("./Assets/add-image.png");
 export const locationPin = require("./Assets/location-pin.png");
 export const more = require("./Assets/more.png");
 export const addUser = require("./Assets/add-user.png");
 export const block = require("./Assets/block.png");
+export const bin = require("./Assets/delete.png");
+export const mail = require("./Assets/mail.png");
