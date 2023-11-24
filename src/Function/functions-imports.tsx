e8e05b163f8bde83e2a11c5b00497c82153337ec
lucas pozzi <lucaspozzif20@gmail.com>
2023-11-24 11:22:00 -0300

    SCHED-9 new function and component imports updated

--- src/Function/functions-imports.tsx
+++ src/Function/functions-imports.tsx
@@ -1,4 +1,5 @@
 import { handleImageInput, onClickRef } from "./click-handlers/on-click-ref";
 import { stateSwitcher } from "./stateSwitcher/stateSwitcher";
+import { formatArray } from "./formatting/format-array/format-array";
 
-export { handleImageInput, onClickRef, stateSwitcher };
+export { handleImageInput, onClickRef, stateSwitcher, formatArray };
