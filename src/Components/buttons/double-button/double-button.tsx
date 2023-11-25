06b2302bee2465fb555779b826021e0d9a0a3fdf
lucas pozzi <lucaspozzif20@gmail.com>
2023-11-25 16:48:34 -0300

    SCHED-18 double button minor changes

--- src/Components/buttons/double-button/double-button.tsx
+++ src/Components/buttons/double-button/double-button.tsx
@@ -1,19 +1,19 @@
 import "./double-button.css";
 
 type DoubleButtonType = {
-  title: string[];
-  onClick: [() => void, () => void];
-  hidden: boolean[];
+  title?: string[];
+  onClick?: [() => void, () => void];
+  hidden?: boolean[];
 };
 
 export function DoubleButton({ title, onClick, hidden }: DoubleButtonType) {
   return (
     <div className='double-button-div'>
-      <p className={"double-button" + (hidden[0] ? " hidden" : "")} onClick={onClick[0]}>
-        {title[0]}
+      <p className={"double-button" + (hidden?.[0] ? " hidden" : "")} onClick={onClick?.[0]}>
+        {title?.[0]}
       </p>
-      <p className={"double-button" + (hidden[1] ? " hidden" : "")} onClick={onClick[1]}>
-        {title[1]}
+      <p className={"double-button" + (hidden?.[1] ? " hidden" : "")} onClick={onClick?.[1]}>
+        {title?.[1]}
       </p>
     </div>
   );
