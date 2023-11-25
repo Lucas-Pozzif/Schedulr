2dd8439b1bd1e80a5906dedbb1d0e191d3b252f5
lucas pozzi <lucaspozzif20@gmail.com>
2023-11-25 13:33:02 -0300

    SCHED-9 link button can now hide

--- src/Components/buttons/link-button/link-button.tsx
+++ src/Components/buttons/link-button/link-button.tsx
@@ -2,12 +2,13 @@ import "./link-button.css";
 type linkButtonType = {
   title: string;
   onClick?: () => void;
+  hidden?: boolean;
 };
 const arrow = require("../../../Assets/arrow.png");
 
-export function LinkButton({ title, onClick }: linkButtonType) {
+export function LinkButton({ title, hidden = false, onClick }: linkButtonType) {
   return (
-    <div className='link-button' onClick={onClick}>
+    <div className={"link-button" + (hidden ? " hidden" : "")} onClick={onClick}>
       <p className='lb-title'>{title}</p>
       <img className={"sched-arrow right"} src={arrow} />
     </div>
