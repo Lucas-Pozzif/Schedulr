ffb14b4e5739316b665d987b1122fd7fcb61c7e2
lucas pozzi <lucaspozzif20@gmail.com>
2023-11-28 11:02:07 -0300

    SCHED-20 new loading screens

---
+++ src/Components/loading/home-banner-loading/home-banner-loading.tsx
@@ -0,0 +1,9 @@
+import "./home-banner-loading.css";
+
+export function HomeBannerLoading() {
+  return (
+    <div className='loading-screen'>
+      <div className='spinner'></div>
+    </div>
+  );
+}
---
+++ src/Components/loading/home-header-loading/home-header-loading.tsx
@@ -0,0 +1,9 @@
+import "./home-header-loading.css";
+
+export function HomeHeaderLoading() {
+  return (
+    <div className='loading-screen'>
+      <div className='spinner'></div>
+    </div>
+  );
+}
