95a2ac39435fb88b70136f86b9a85ef0cbdfcdcf
lucas pozzi <lucaspozzif20@gmail.com>
2023-11-28 11:02:22 -0300

    SCHED-20 updated component imports

---
+++ src/Components/component-imports.ts
@@ -0,0 +1,16 @@
+import { ReturnButton } from "./buttons/return-button/return-button";
+import { SmallHeader } from "./headers/small-header/small-header";
+import { UserProfile } from "./user-profile/user-profile";
+import { IconButton } from "./buttons/icon-button/icon-button";
+import { UserPageList } from "./lists/user-page-list/user-page-list";
+import { SmallIconButton } from "./buttons/small-item-button/small-icon-button";
+import { UserPageLoading } from "./loading/user-page-loading/user-page-loading";
+import { VerticalLine, Line, ThickLine } from "./line/line";
+import { GroupList } from "./lists/group-list/group-list";
+import { HomeHeader } from "./headers/home-header/home-header";
+import { HomePageLoading } from "./loading/home-page-loading/home-page-loading";
+import { HomeSearchBar } from "./inputs/home-searchbar/home-searchbar";
+import { WhiteButton } from "./buttons/white-button/white-button";
+import { HomeBanner } from "./home-banner/home-banner";
+
+export { ReturnButton, ThickLine, HomeSearchBar, HomeBanner, HomePageLoading, WhiteButton, HomeHeader, SmallIconButton, VerticalLine, Line, IconButton, GroupList, UserPageLoading, UserPageList, SmallHeader, UserProfile };
