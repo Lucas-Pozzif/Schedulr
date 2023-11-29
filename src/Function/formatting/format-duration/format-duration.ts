15b09578c27d7239e391da26c59c5266682973d3
lucas pozzi <lucaspozzif20@gmail.com>
2023-11-29 17:30:25 -0300

    SCHED-20 format duration now handles no duration

--- src/Function/formatting/format-duration/format-duration.ts
+++ src/Function/formatting/format-duration/format-duration.ts
@@ -1,3 +1,4 @@
-export function formatDuration(duration: boolean[]) {
-    return `${Math.floor(duration.length / 6)}h ${(duration.length % 6) * 10}m`
+export function formatDuration(duration?: boolean[]) {
+  if (!duration) return;
+  return `${Math.floor(duration.length / 6)}h ${(duration.length % 6) * 10}m`;
 }
