d4780d3d25122e3a7cea8953949a3646e5583ec3
lucas pozzi <lucaspozzif20@gmail.com>
2023-11-29 13:19:40 -0300

    SCHED-20 component imports updated

--- src/Components/component-imports.ts
+++ src/Components/component-imports.ts
@@ -2,7 +2,7 @@ import { ReturnButton } from "./buttons/return-button/return-button";
 import { SmallHeader } from "./headers/small-header/small-header";
 import { UserProfile } from "./user-profile/user-profile";
 import { IconButton } from "./buttons/icon-button/icon-button";
-import { UserPageList } from "./lists/user-page-list/user-page-list";
+import { IconList } from "./lists/user-page-list/icon-list";
 import { SmallIconButton } from "./buttons/small-item-button/small-icon-button";
 import { UserPageLoading } from "./loading/user-page-loading/user-page-loading";
 import { VerticalLine, Line, ThickLine } from "./line/line";
@@ -11,6 +11,31 @@ import { HomeHeader } from "./headers/home-header/home-header";
 import { HomePageLoading } from "./loading/home-page-loading/home-page-loading";
 import { HomeSearchBar } from "./inputs/home-searchbar/home-searchbar";
 import { WhiteButton } from "./buttons/white-button/white-button";
-import { HomeBanner } from "./home-banner/home-banner";
+import { HomeBanner } from "./banners/home-banner/home-banner";
+import { GroupHeader } from "./headers/group-header/group-header";
+import { LinkList } from "./lists/link-list/link-list";
+import { GenericHeader } from "./headers/generic-header/generic-header";
+import {Carousel} from './carousels/carousel/carousel'
 
-export { ReturnButton, ThickLine, HomeSearchBar, HomeBanner, HomePageLoading, WhiteButton, HomeHeader, SmallIconButton, VerticalLine, Line, IconButton, GroupList, UserPageLoading, UserPageList, SmallHeader, UserProfile };
+export {
+  Carousel,
+  ReturnButton,
+  LinkList,
+  GenericHeader,
+  GroupHeader,
+  ThickLine,
+  HomeSearchBar,
+  HomeBanner,
+  HomePageLoading,
+  WhiteButton,
+  HomeHeader,
+  SmallIconButton,
+  VerticalLine,
+  Line,
+  IconButton,
+  GroupList,
+  UserPageLoading,
+  IconList,
+  SmallHeader,
+  UserProfile,
+};
