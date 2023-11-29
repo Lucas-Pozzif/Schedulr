4ba5a735d63743902a58707405dc9180aae66a31
lucas pozzi <lucaspozzif20@gmail.com>
2023-11-29 17:28:52 -0300

    SCHED-20 generic header changes

--- src/Components/headers/generic-header/generic-header.tsx
+++ src/Components/headers/generic-header/generic-header.tsx
@@ -3,16 +3,17 @@ import "./generic-header.css";
 
 type GenericHeaderType = {
   title: string;
-  icon: string;
+  icon?: string;
   onClickReturn: () => void;
+  onClickIcon?: () => void;
 };
 
-export function GenericHeader({ title, icon, onClickReturn }: GenericHeaderType) {
+export function GenericHeader({ title, icon, onClickReturn, onClickIcon }: GenericHeaderType) {
   return (
     <div className='generic-header'>
       <ReturnButton onClick={onClickReturn} />
       <p className='gh-title'>{title}</p>
-      <img className='gh-icon' src={icon} />
+      <img className='gh-icon' src={icon} onClick={onClickIcon} />
     </div>
   );
 }
