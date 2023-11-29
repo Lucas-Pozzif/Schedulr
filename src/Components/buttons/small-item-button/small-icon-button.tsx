9807d9dc02f35d33d8ed4874ff580f7a73787921
lucas pozzi <lucaspozzif20@gmail.com>
2023-11-29 13:16:51 -0300

    SCHED-20 small item button updated

--- src/Components/buttons/small-item-button/small-icon-button.tsx
+++ src/Components/buttons/small-item-button/small-icon-button.tsx
@@ -3,13 +3,14 @@ import "./small-icon-button.css";
 type SmallIconButtonType = {
   title: string;
   icon: string;
+  select?: boolean;
   hide?: boolean;
   onClick: () => void;
 };
 
-export function SmallIconButton({ title, hide, icon, onClick }: SmallIconButtonType) {
+export function SmallIconButton({ title, hide, select, icon, onClick }: SmallIconButtonType) {
   return (
-    <div className={`small-icon-button${hide ? "-hide" : ""}`} onClick={onClick}>
+    <div className={`small-icon-button${hide ? "-hide" : select ? "-select" : ""}`} onClick={onClick}>
       <img className='sib-icon' src={icon} />
       <p className='sib-title'>{title}</p>
     </div>
