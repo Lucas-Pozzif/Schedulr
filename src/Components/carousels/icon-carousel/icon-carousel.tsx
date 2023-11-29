580773eac72aac03c6bf4b65858f234397241175
lucas pozzi <lucaspozzif20@gmail.com>
2023-11-29 13:17:21 -0300

    SCHED-20 icon carousel created

---
+++ src/Components/carousels/icon-carousel/icon-carousel.tsx
@@ -0,0 +1,21 @@
+import { SmallIconButton } from "../../component-imports";
+import "./icon-carousel.css";
+
+type IconCarouselType = {
+  items: {
+    title: string;
+    icon: string;
+    onClick: () => void;
+    select: boolean;
+  }[];
+};
+
+export function IconCarousel({ items }: IconCarouselType) {
+  return (
+    <div className='icon-carousel'>
+      {items.map((item) => {
+        return <SmallIconButton title={item.title} select={item.select} icon={item.icon} onClick={item.onClick} />;
+      })}
+    </div>
+  );
+}
