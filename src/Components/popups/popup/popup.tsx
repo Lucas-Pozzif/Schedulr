4cf7b68eb0ef65764ecd6c5cb202219fb0beffa3
lucas pozzi <lucaspozzif20@gmail.com>
2023-11-30 08:39:13 -0300

    SCHED-20 popup done

---
+++ src/Components/popups/popup/popup.tsx
@@ -0,0 +1,30 @@
+import "./popup.css";
+
+type PopupType = {
+  title: string;
+  text: string;
+  onClickExit: () => void;
+  buttons: {
+    title: string;
+    onClick: () => void;
+  }[];
+};
+
+export function Popup({ title, text, onClickExit, buttons }: PopupType) {
+  return (
+    <div className='popup'>
+      <p className='p-title'>{title}</p>
+      <p className='p-text'>{text}</p>
+      <div className='p-button-list'>
+        {buttons.map((button) => {
+          return (
+            <div className='p-button' onClick={button.onClick}>
+              <p className='p-button-title'>{button.title}</p>
+            </div>
+          );
+        })}
+      </div>
+      <img className='p-exit' src={closeIcon} onClick={onClickExit} />
+    </div>
+  );
+}
