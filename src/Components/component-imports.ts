5eb4a26cf10f1bd148a4e5d9ce0a9e6732f7ed12
lucas pozzi <lucaspozzif20@gmail.com>
2023-12-01 19:02:21 -0300

    SCHED-20 imports alphabetically organized

--- src/Components/component-imports.ts
+++ src/Components/component-imports.ts
@@ -1,63 +1,69 @@
-import { ReturnButton } from "./buttons/return-button/return-button";
-import { SmallHeader } from "./headers/small-header/small-header";
-import { UserProfile } from "./user-profile/user-profile";
-import { IconButton } from "./buttons/icon-button/icon-button";
-import { IconList } from "./lists/user-page-list/icon-list";
-import { SmallIconButton } from "./buttons/small-item-button/small-icon-button";
-import { UserPageLoading } from "./loading/user-page-loading/user-page-loading";
-import { VerticalLine, Line, ThickLine } from "./line/line";
-import { GroupList } from "./lists/group-list/group-list";
-import { HomeHeader } from "./headers/home-header/home-header";
-import { HomePageLoading } from "./loading/home-page-loading/home-page-loading";
+import { HeaderInput } from "./inputs/header-input/header-input";
 import { HomeSearchBar } from "./inputs/home-searchbar/home-searchbar";
-import { WhiteButton } from "./buttons/white-button/white-button";
-import { HomeBanner } from "./banners/home-banner/home-banner";
-import { GroupHeader } from "./headers/group-header/group-header";
+import { IconList } from "./lists/user-page-list/icon-list";
+import { ItemList } from "./lists/item-list/item-list";
 import { LinkList } from "./lists/link-list/link-list";
-import { GenericHeader } from "./headers/generic-header/generic-header";
+import { DualList } from "./lists/dual-list/dual-list";
+import { GroupList } from "./lists/group-list/group-list";
 import { Carousel } from "./carousels/carousel/carousel";
 import { IconCarousel } from "./carousels/icon-carousel/icon-carousel";
-import { ItemList } from "./lists/item-list/item-list";
-import { SubHeader } from "./headers/sub-header/sub-header";
-import { BottomPopup } from "./popups/bottom-popup/bottom-popup";
-import { GroupBanner } from "./banners/group-banner/group-banner";
-import { GroupFormLoading } from "./loading/group-form-loading/group-form-loading";
-import { HeaderInput } from "./inputs/header-input/header-input";
-import { ServiceFormLoading } from "./loading/service-form-loading/service-form-loading";
 import { Popup } from "./popups/popup/popup";
+import { BottomPopup } from "./popups/bottom-popup/bottom-popup";
 import { BottomButton } from "./buttons/bottom-button/bottom-button";
+import { DoubleButton } from "./buttons/double-button/double-button";
+import { ReturnButton } from "./buttons/return-button/return-button";
+import { IconButton } from "./buttons/icon-button/icon-button";
+import { SmallIconButton } from "./buttons/small-item-button/small-icon-button";
+import { WhiteButton } from "./buttons/white-button/white-button";
+import { GroupBanner } from "./banners/group-banner/group-banner";
+import { HomeBanner } from "./banners/home-banner/home-banner";
+import { GenericHeader } from "./headers/generic-header/generic-header";
+import { GroupHeader } from "./headers/group-header/group-header";
+import { HomeHeader } from "./headers/home-header/home-header";
+import { SmallHeader } from "./headers/small-header/small-header";
+import { SubHeader } from "./headers/sub-header/sub-header";
+import { SchedulePageLoading } from "./loading/schedule-page-loading/schedule-page-loading";
+import { HomePageLoading } from "./loading/home-page-loading/home-page-loading";
+import { GroupFormLoading } from "./loading/group-form-loading/group-form-loading";
+import { UserPageLoading } from "./loading/user-page-loading/user-page-loading";
 import { ProfessionalFormLoading } from "./loading/professional-form-loading/professional-form-loading";
+import { ServiceFormLoading } from "./loading/service-form-loading/service-form-loading";
+import { Profile } from "./profile/profile";
+import { VerticalLine, Line, ThickLine } from "./line/line";
 
 export {
-  BottomButton,
   HeaderInput,
-  ServiceFormLoading,
-  Popup,
-  ProfessionalFormLoading,
-  IconCarousel,
+  HomeSearchBar,
+  IconList,
   ItemList,
-  SubHeader,
-  BottomPopup,
-  GroupBanner,
-  GroupFormLoading,
-  Carousel,
-  ReturnButton,
   LinkList,
+  DualList,
+  GroupList,
+  Carousel,
+  IconCarousel,
+  Popup,
+  BottomPopup,
+  BottomButton,
+  DoubleButton,
+  ReturnButton,
+  IconButton,
+  SmallIconButton,
+  WhiteButton,
+  GroupBanner,
+  HomeBanner,
   GenericHeader,
   GroupHeader,
-  ThickLine,
-  HomeSearchBar,
-  HomeBanner,
-  HomePageLoading,
-  WhiteButton,
   HomeHeader,
-  SmallIconButton,
+  SmallHeader,
+  SubHeader,
+  SchedulePageLoading,
+  HomePageLoading,
+  GroupFormLoading,
+  UserPageLoading,
+  ProfessionalFormLoading,
+  ServiceFormLoading,
+  Profile,
   VerticalLine,
   Line,
-  IconButton,
-  GroupList,
-  UserPageLoading,
-  IconList,
-  SmallHeader,
-  UserProfile,
+  ThickLine,
 };
