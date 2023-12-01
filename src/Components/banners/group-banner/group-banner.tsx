373bcfb17f771b16c70f823df47ba70e4402472f
lucas pozzi <lucaspozzif20@gmail.com>
2023-12-01 10:23:03 -0300

    SCHED-20 group banner small changes

--- src/Components/banners/group-banner/group-banner.tsx
+++ src/Components/banners/group-banner/group-banner.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import "./group-banner.css";
 
 type GroupBannerType = {
@@ -14,14 +15,21 @@ const addImage = require("../../../Assets/add-image.png");
 const arrow = require("../../../Assets/arrow.png");
 
 export function GroupBanner({ banner, profile, onClickProfile, onClickBanner, onClickReturn, returnButton, placeholder = addImage }: GroupBannerType) {
+  const navigate = useNavigate();
   return (
-    <div className='group-banner' onClick={onClickBanner}>
-      <img className={`group-banner-${banner ? "image" : "placeholder"}`} src={banner ? banner : placeholder} />
+    <div className='group-banner'>
+      <img className={`group-banner-${banner ? "image" : "placeholder"}`} src={banner ? banner : placeholder} onClick={onClickBanner} />
       <div className='gp-blur' />
-      <img className={`return-button gp-return ${returnButton ? "" : "hidden"}`} src={arrow} onClick={onClickReturn} />
       <div className='gp-profile' onClick={onClickProfile}>
         <img className={`gp-profile-${profile ? "image" : "placeholder"}`} src={profile ? profile : addImage} />
       </div>
+      <img
+        className={`return-button gp-return ${returnButton ? "" : "hidden"}`}
+        src={arrow}
+        onClick={() => {
+          onClickReturn ? onClickReturn() : navigate(-1);
+        }}
+      />
     </div>
   );
 }
