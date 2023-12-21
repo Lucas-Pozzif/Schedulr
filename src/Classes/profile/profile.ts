a4441823d0f0c16b025d84c8c6e83c80c4cba324
lucas pozzi <lucaspozzif20@gmail.com>
2023-12-21 14:42:55 -0300

    SCHED-29 changes

--- src/Classes/profile/profile.ts
+++ src/Classes/profile/profile.ts
@@ -1,7 +1,11 @@
+import { DocumentData, DocumentSnapshot } from "firebase/firestore";
 import { Schedule } from "../classes-imports";
+import { storage } from "../../Services/firebase/firebase";
+import { getDownloadURL, ref } from "firebase/storage";
 
 export class Profile {
   private _id: string;
+  private _groupId: string;
   private _name: string;
   private _occupations: { name: string; id: string }[];
   private _number: string;
@@ -14,6 +18,7 @@ export class Profile {
 
   constructor(
     arg?: string | Profile,
+    groupId: string = "",
     name: string = "",
     occupations: { name: string; id: string }[] = [],
     number: string = "",
@@ -27,6 +32,7 @@ export class Profile {
     if (typeof arg === "string") {
       // Case: ID provided
       this._id = arg;
+      this._groupId = groupId;
       this._name = name;
       this._occupations = occupations;
       this._number = number;
@@ -38,8 +44,9 @@ export class Profile {
       this._schedule = schedule;
     } else if (arg instanceof Profile) {
       // Case: Another Professional object provided
-      const { _id, _name, _occupations, _number, _isAdmin, _activities, _hours, _startHours, _profile, _schedule } = arg;
+      const { _id, _groupId, _name, _occupations, _number, _isAdmin, _activities, _hours, _startHours, _profile, _schedule } = arg;
       this._id = _id;
+      this._groupId = _groupId;
       this._name = _name;
       this._occupations = _occupations;
       this._number = _number;
@@ -52,6 +59,7 @@ export class Profile {
     } else {
       // Case: No arguments or invalid argument type
       this._id = "";
+      this._groupId = groupId;
       this._name = name;
       this._occupations = occupations;
       this._number = number;
@@ -63,4 +71,43 @@ export class Profile {
       this._schedule = schedule;
     }
   }
+
+  // Database comunication
+
+  public fillProfile(profSnap: DocumentSnapshot<DocumentData, DocumentData>) {
+    if (!profSnap.data()) console.error("no data was found, the id is incorrect or this profile was not created yet");
+    else {
+      const profData = profSnap.data();
+
+      this._id = profSnap.id;
+      this._name = profData?.name || "";
+      this._occupations = profData?.occupations || [];
+      this._number = profData?.number || "";
+      this._isAdmin = profData?.isAdmin || false;
+      this._activities = profData?.activities || [];
+      this._hours = profData?.hours || [profData?.hours[0] || [], profData?.hours[1] || [], profData?.hours[2] || [], profData?.hours[3] || [], profData?.hours[4] || [], profData?.hours[5] || [], profData?.hours[6] || []];
+      this._startHours = profData?.startHours || [-1, -1, -1, -1, -1, -1, -1];
+    }
+  }
+
+  // Download from database
+
+  public async getImages() {
+    const profileRef = ref(storage, `groups/${this._groupId}/profiles/${this._id}/profile`);
+    this._profile = await getDownloadURL(profileRef);
+  }
+
+  // Upload to database
+
+  public async addProfile() {}
+
+  // Update the database
+
+  // Remove from the database
+
+
+  /**
+   * Gets the schedule day of this profile, it will grab the entire day
+   */
+  public async getSchedule(day: Date) {}
 }
