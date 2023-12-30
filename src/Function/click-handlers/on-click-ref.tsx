131d9bce5534d8c1347840528ea4b27ceaae7bb6
lucas pozzi <lucaspozzif20@gmail.com>
2023-12-30 16:48:29 -0300

    onClickRef updated for the new

--- src/Function/click-handlers/on-click-ref.tsx
+++ src/Function/click-handlers/on-click-ref.tsx
@@ -6,8 +6,7 @@ export const onClickRef = (reference: React.RefObject<HTMLInputElement>) => {
 export const handleImageInput = async (event: React.ChangeEvent<HTMLInputElement>, group: Group, setGroup: React.Dispatch<React.SetStateAction<Group>>, attribute: "banner" | "profile") => {
   const selectedFile = event.target.files?.[0];
   if (selectedFile) {
-    if (attribute == "banner") group.setBanner(URL.createObjectURL(selectedFile));
-    else if (attribute == "profile") group.setProfile(URL.createObjectURL(selectedFile));
-    setGroup(new Group(group));
+    if (attribute == "banner") group.updateValue("banner", URL.createObjectURL(selectedFile), false, setGroup);
+    else if (attribute == "profile") group.updateValue("profile", URL.createObjectURL(selectedFile), false, setGroup);
   }
 };
--- src/Function/switchers/id-switcher/id-switcher.ts
+++ src/Function/switchers/id-switcher/id-switcher.ts
@@ -1,7 +1,7 @@
 export const idSwitcher = (value: any, newValue: any, setter: React.Dispatch<React.SetStateAction<any>>) => {
-    if (value?.getId() === newValue?.getId()) {
-        setter(null);
-    } else {
-        setter(newValue);
-    }
+  if (value?.get("id") === newValue?.get("id")) {
+    setter(null);
+  } else {
+    setter(newValue);
+  }
 };
