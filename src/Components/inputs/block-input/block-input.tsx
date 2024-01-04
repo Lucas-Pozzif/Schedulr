28f0505ae9f39fdd73f682d46c070c2c15d9b1a7
lucas pozzi <lucaspozzif20@gmail.com>
2024-01-04 12:53:32 -0300

    SCHED-34 block input created

---
+++ src/Components/inputs/block-input/block-input.tsx
@@ -0,0 +1,19 @@
+import "./block-input.css";
+
+type BlockInputType = {
+  placeholder?: string;
+  value?: string;
+  label?: string;
+  type?: string;
+  maxLength?: number;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export function BlockInput({ placeholder, value, label, type, maxLength = 126, onChange }: BlockInputType) {
+  return (
+    <form className='block-input'>
+      <label className='bi-label'>{label}</label>
+      <input className='bi-input' type={type} maxLength={maxLength} placeholder={placeholder} value={value} onChange={onChange}></input>
+    </form>
+  );
+}
