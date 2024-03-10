38ee3576793f8f255e748c0b2b5c16addc952ba0
lucas pozzi <lucaspozzif20@gmail.com>
2024-03-10 16:33:38 -0300

    SCHED-39 class path updated

--- src/Classes/classpaths.ts
+++ src/Classes/classpaths.ts
@@ -1,4 +1,6 @@
 import { GroupForm } from "./group/group-form";
 import { Group } from "./group/group-class";
+import { Activity } from "./activity/activity-class";
+import { Profile } from "./profile/profile-class";
 
-export { GroupForm, Group };
+export { GroupForm, Group, Activity, Profile };
