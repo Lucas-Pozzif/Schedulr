f5479bbe79a3fe45c75f773d51986d3b2b54d797
lucas pozzi <lucaspozzif20@gmail.com>
2024-03-09 21:43:34 -0300

    SCHED-39 routes page cleanup

--- src/_routes.tsx
+++ src/_routes.tsx
@@ -1,18 +1,7 @@
-import { GroupForm } from "./Forms/group-form/group-form";
-import { ErrorPage } from "./Pages/error-page/error-page";
-import { GroupPage } from "./Pages/group-page/group-page";
-import { Home } from "./Pages/home/home";
-import { ClientSchedulePage } from "./Pages/schedule-page/client-schedule/client-schedule-page";
-import { ProfessionalSchedulePage } from "./Pages/schedule-page/professional-schedule/professional-schedule-page";
-import { UserPage } from "./Pages/user-page/user-page";
+import { GroupForm } from "./Classes/classpaths";
 
 export const routes = [
-  { path: "/", element: <Home /> },
-  { path: "/user", element: <UserPage /> },
-  { path: "/user/schedule/:userId", element: <ClientSchedulePage /> },
-  { path: "/group/:groupId", element: <GroupPage /> },
+  { path: "/", element: <GroupForm /> },
   { path: "/group/add", element: <GroupForm /> },
-  { path: "/group/edit/:groupId", element: <GroupForm /> },
-  { path: "/professional/schedule/:professionalId", element: <ProfessionalSchedulePage /> },
-  { path: "/error", element: <ErrorPage /> },
+  { path: "/group/edit/:id", element: <GroupForm /> },
 ];
