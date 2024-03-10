cd253fe094f89fe82926f2f3fe04345498202024
lucas pozzi <lucaspozzif20@gmail.com>
2024-03-10 16:34:33 -0300

    SCHED-39 new global imports

---
+++ src/_global.ts
@@ -0,0 +1,9 @@
+export const icon = {
+  arrow: require("./Assets/arrow.png"),
+  calendar: require("./Assets/calendar.png"),
+  edit: require("./Assets/edit.png"),
+  addImage: require("./Assets/add-image.png"),
+  clock: require("./Assets/clock.png"),
+  logout: require("./Assets/logout.png"),
+  user: require("./Assets/edit.png"),
+};
